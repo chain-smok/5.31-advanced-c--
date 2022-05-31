12c0df1e9545975be877d1dc8dc69842d06b5940
chain-smok <s1101780@gm.pu.edu.tw>
2022-05-31 16:04:06 +0800

    debuginherit

--- Personexample.cpp
+++ Personexample.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
-class Person{
-   private:
+class Person {
+   protected:
      int id;
      string name;
    public:
       Person():id(0),name(""){};  ///Person p1;
       Person(int tid,string tname):id(tid),name(tname){};//Person p1(1,"john");
+      Person(Person &p){
+          id=p.id;
+          name=p.name;
+      }
       int getid(){return id;}
       string getName(){return name;}
       void setId(int tid){
@@ -25,18 +29,20 @@ class Student:public Person{
    public:
       Student():Person(),gpa(0.0){};//Student s1;
       Student(int tid,string tname,double tgpa):Person(tid,tname),gpa(tgpa){}
+      Student(Student &s):Person(s.id,s.name),gpa(s.gpa){}
       void setGpa(double tgpa){
           gpa=tgpa;
       }
       double getGpa(){return gpa;}
       void print(){//overwrite parent
-          cout<<"ID="<<getid()<<" ,name="<<getName()<<" ,GPA="<<getGpa()<<endl;
+          cout<<"ID="<<id<<" ,name="<<name<<" ,GPA="<<gpa<<endl;
       }
 
 };
 int main(){
-    Student s1,s2(1,"mary",3.5),s3(2,"john",4.0);
+    Student s1,s2(1,"mary",3.5),s3(2,"john",4.0),s4(s2);
     s1.print();
     s2.print();
     s3.print();
+    s4.print();
 }
